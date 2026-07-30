3e8e7a69a234047b3c99fdc41d60a78fd5a4c8c9
ciitinstitutepune <ciitpune@gmail.com>
2026-07-30 20:21:27 +0530

    added files  in the engineering branch

---
+++ StudentOperations/Engineering/MechanicalDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentOperations.Engineering
+{
+    internal class MechanicalDepartment
+    {
+        public void DepartmentDetails()
+        {
+            Console.WriteLine("Department Details");
+        }
+    }
+}
